7ecc84be99b4096bcb92297f30f2b2894f8b70c6
Dr3a0p <dreandreax@icloud.com>
2023-09-30 18:48:10 -0500

    completed About Me

--- develop/my-app/src/components/pages/About.js
+++ develop/my-app/src/components/pages/About.js
@@ -5,7 +5,8 @@ export default function About() {
     <div>
       <h1>About Me</h1>
       <p>
-    As you already know, my name is Andrea Castillo
+    As you already know, my name is Andrea Castillo. I attend UT Austin bootcamp to learn and gain knowledge of what skills it takes to complete functional websites, how to make the correct connections such as APIs and knowing how to use databases for back-end functionality to get your sites up and running perfectly well.
+    The skills I have aqcuired so far have been html, CSS, Javascript, OOP, node.js, express.js, React, API post get/requests, SQL, ORM and ect. there is so much more but the point is to present these skills to you so you can see for yourself! I'm very excited to continue gaining more knowledge and experience through potential teamates!
       </p>
     </div>
   );
