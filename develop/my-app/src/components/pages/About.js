7b3126a96183e153289d5b2da8093f31524b93fe
Dr3a0p <dreandreax@icloud.com>
2023-09-30 18:39:46 -0500

    fixed

---
+++ develop/my-app/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+    As you already know, my name is Andrea Castillo
+      </p>
+    </div>
+  );
+}
---
+++ develop/my-app/src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Porfolio!</h1>
+      <p>
+      Hello my name is Andrea Castillo & I am a full-stack web developer!
+      </p>
+    </div>
+  );
+}
