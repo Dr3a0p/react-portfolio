921f33bba07fb9283746e53ae5b6f15e8e477cdd
Dr3a0p <dreandreax@icloud.com>
2023-09-30 19:47:15 -0500

    descriptions

--- develop/my-app/src/components/pages/Blog.js
+++ develop/my-app/src/components/pages/Blog.js
@@ -3,21 +3,20 @@ import React from 'react';
 export default function Blog() {
   return (
     <div>
-      <h1>Blog Page</h1>
+      <h1>Project Page</h1>
       <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
+      These are a couple of projects i worked on with really great classmates.
+
+1. What 2 Eat/ Tasty
+this was a very fun project to work on not only did this project teach me 
+how to proactively code within an interconnected work area but I was also able to practice API requests. This application gives you the choice to eat in or find a place nearby using google maps to locate resturaunts around your area! If you choose to eaat in, you'll simply type in and search for whatever you're craving. An example would be maybe you're in the mood for chicken. After clicking the search button, you'll be given recipes you can cook from home. Maybe there's a recipe you want to save for later, you're also given the exact link towards said recipe! 
+</p>
+
+<p>
+
+2.Floral Frenzy!
+This was more of a playful, fun creative project where we used MYSQL database to input and save data for our functionality. We also incorporated Handlebars.js for styling and lastly Node.js and Express.js to create a RESTful API.
+This application's storyline is allowing the user to play a match game where the user is a gardener matching up flowers from their garden by symbols and colors! If there are multiple users playing they may also have the choice to chat using our chatbox. When the game has ended it'll throw out the highest scores with the users names!
       </p>
     </div>
   );
--- develop/my-app/src/components/pages/Contact.js
+++ develop/my-app/src/components/pages/Contact.js
@@ -3,19 +3,20 @@ import React from 'react';
 export default function Contact() {
   return (
     <div>
-      <h1>Contact Page</h1>
+      <h1>Contact Info</h1>
       <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+       Here are a few ways you can reach me!
+       </p>
+       <p>
+
+       1.Email:
+       </p>
+       <p>
+
+       2.Work Mobile Number:
+</p>
+<p>
+       3. 
       </p>
     </div>
   );
